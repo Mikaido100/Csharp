760fbf3103065bf806528694733d6d7f8ddd4e42
3110481 <3110481@TE-GDA-D-D37H04>
2022-03-23 13:43:29 +0100

    Dodaj pliki projektów.

---
+++ clicker/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.upgradeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonLevelTextBox = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(507, 346);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "NIE KLIKAJ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // upgradeButton
+            // 
+            this.upgradeButton.Location = new System.Drawing.Point(380, 379);
+            this.upgradeButton.Name = "upgradeButton";
+            this.upgradeButton.Size = new System.Drawing.Size(75, 36);
+            this.upgradeButton.TabIndex = 1;
+            this.upgradeButton.Text = "Upgrade";
+            this.upgradeButton.UseVisualStyleBackColor = true;
+            this.upgradeButton.Click += new System.EventHandler(this.upgradeButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(551, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // buttonLevelTextBox
+            // 
+            this.buttonLevelTextBox.Location = new System.Drawing.Point(380, 346);
+            this.buttonLevelTextBox.Name = "buttonLevelTextBox";
+            this.buttonLevelTextBox.Size = new System.Drawing.Size(100, 23);
+            this.buttonLevelTextBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonLevelTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.upgradeButton);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button upgradeButton;
+        private Label label1;
+        private TextBox buttonLevelTextBox;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+    }
+}
